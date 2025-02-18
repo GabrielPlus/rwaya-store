52a9e7254fd99069d182892fbb5e982000758bef
GabrielPlus <gabrielpius0311@gmail.com>
2025-02-18 23:19:01 +0300

    prod

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,18 +8,52 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("85bc7190-88ba-45c0-9151-ec018453249c");
+  const billboards = await getBillboard("85bc7190-88ba-45c0-9151-ec018453249c");
 
-  return ( 
+  // Ensure you pass a single billboard object to the Billboard component
+  const billboard = billboards[0]; // Use the first billboard in the array
+
+  if (!billboard) {
+    return <div>No billboard found</div>;
+  }
+
+  return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
-      </div>
+        </div>
       </div>
     </Container>
-   );
-}
- 
+  );
+};
+
 export default HomePage;
+
+
+// import getBillboard from "@/actions/get-billboard";
+// import getProducts from "@/actions/get-products";
+// import Billboard from "@/components/billboard";
+// import ProductList from "@/components/product-list";
+// import Container from "@/components/ui/container";
+
+// export const revalidate = 0;
+
+// const HomePage = async () => {
+//   const products = await getProducts({ isFeatured: true });
+//   const billboard = await getBillboard("85bc7190-88ba-45c0-9151-ec018453249c");
+
+//   return ( 
+//     <Container>
+//       <div className="space-y-10 pb-10">
+//         <Billboard data={billboard} />
+//       <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+//           <ProductList title="Featured Products" items={products} />
+//       </div>
+//       </div>
+//     </Container>
+//    );
+// }
+ 
+// export default HomePage;
