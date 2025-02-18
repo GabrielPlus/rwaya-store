30d01e8ce31e11ebcc23677dc783d7554c5fd22b
GabrielPlus <gabrielpius0311@gmail.com>
2025-02-18 23:38:33 +0300

    bill

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,14 +8,24 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboards = await getBillboard("85bc7190-88ba-45c0-9151-ec018453249c");
+  const billboards = await getBillboard("11a927c8-ffc9-4493-bc7d-ffd5fa6b3a25");
+
+  // Handle the case where billboards is null or undefined
+  if (!billboards || billboards.length === 0) {
+    return (
+      <Container>
+        <div className="space-y-10 pb-10">
+          <div>No billboard found</div>
+          <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+            <ProductList title="Featured Products" items={products} />
+          </div>
+        </div>
+      </Container>
+    );
+  }
 
   // Ensure you pass a single billboard object to the Billboard component
   const billboard = billboards[0]; // Use the first billboard in the array
-
-  if (!billboard) {
-    return <div>No billboard found</div>;
-  }
 
   return (
     <Container>
@@ -30,7 +40,6 @@ const HomePage = async () => {
 };
 
 export default HomePage;
-
 
 // import getBillboard from "@/actions/get-billboard";
 // import getProducts from "@/actions/get-products";
